582a360defdd3ebe78e599264ecfbf879656ca03
henrylaufrans <henry.laufrans@student.umn.ac.id>
2021-12-14 23:45:54 +0700

    ganti sc

--- covid-19-main/src/pages/AboutUs.js
+++ covid-19-main/src/pages/AboutUs.js
@@ -7,23 +7,23 @@ function AboutUs() {
 
       <div className="row">
         <div className="col-12">
-           <p className="mb-5 fw-bold text-center fs-3">About Us</p>
+           <p className="mb-5 fw-bold text-center fs-3">About Us - Kelompok 4</p>
            
         </div>
       </div>
       <div className="row text-center">
         <div className="col-12 col-md-6">
-          <img className="rounded-circle mb-2" width="140" height="140"  role="img" src="images/henry"/>
+          <img className="rounded-circle mb-2" width="140" height="140"  role="img" src="charlie.jpg"/>
 
-          <h2>Nama</h2>
-          <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+          <h2>Charlie Frederico</h2>
+          <p>00000043442</p>
         </div>
 
         <div className="col-12 col-md-6">
-         <img className="rounded-circle mb-2" width="140" height="140"  role="img" src="covid-19-main/images/charlie"/>
+         <img className="rounded-circle mb-2" width="140" height="140"  role="img" src="henry.jpg"/>
 
-          <h2>Nama</h2>
-          <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+          <h2>Henry Laufrans</h2>
+          <p>00000042914</p>
         </div>
       </div>
     </div>
